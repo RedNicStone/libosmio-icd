bcff4438e759bbd66ba19c7a765d2d2eca0e29c1
RedNicStone <nic@struktur.de>
2023-08-22 00:46:25 +0200

    Fixed some CMake issues relating to configuring

--- platform/unix/src/icd.cpp
+++ platform/unix/src/icd.cpp
@@ -1,3 +1,10 @@
 //
 // Created by RedNicStone on 22/08/23.
 //
+
+#include <libosmio-loader/entrypoint.h>
+#include <>
+
+void os_icdGetGlobalDispatchTable(Os_icdGlobalDispatchTable** ppDispatchTable) {
+    ppDispatchTable = nullptr;
+}
